5bd28d8e634f2ca8cda0744a0fbc5a2215943fa5
Labs-DECC <Labs-DECC@10.9.5.46>
2016-09-22 18:50:02 -0500

    Add Hamcrest - Netbeans 8.0.2

---
+++ src/ec/edu/espe/math/geometry/solids/cylinders/CylinderSolidInterface.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espe.math.geometry.solids.cylinders;
+
+import ec.edu.espe.math.geometry.solids.SolidInterface;
+
+/**
+ *
+ * @author Cazaesclavas
+ */
+public interface CylinderSolidInterface extends SolidInterface{
+    
+}
